753192f58707ea4e2c45f3ae177d3d9c3194a83c
Hiran <hiran@gluent.com>
2023-12-02 01:19:38 -0500

    chore: cleanup

--- setup/utils/api.py
+++ setup/utils/api.py
@@ -8,24 +8,29 @@ current_year = datetime.now().year
 current_day = datetime.now().day
 current_directory = os.getcwd()
 
-session_cookie = os.path.join(current_directory, "setup/session.cookie")
+print(f"Current directory is {current_directory}")
 
-def get_session_id(filename):
-    if not exists(filename):
-        # Handle the error: either create the file or raise an error
-        raise FileNotFoundError(f"No such file or directory: '{filename}'")
-    with open(filename) as f:
-        session_id = ""
-        # If starts with session=, remove it
-        if f.read().startswith("session="):
-            f.seek(8)
-        session_id = f.read().strip()
-        return session_id
+def get_session_id():
+    # Check if the session cookie exists in environment variable, otherwise use the file
+    if "ADVENT_OF_CODE_SESSION" in os.environ:
+        session_id = os.environ["ADVENT_OF_CODE_SESSION"]
+    else:
+        session_cookie_file = os.path.join(current_directory, "setup/session.cookie")
+        if not exists(session_cookie_file):
+            # Handle the error: either create the file or raise an error
+            raise FileNotFoundError(f"No such file or directory: '{session_cookie_file}'")
+        with open(session_cookie_file) as f:
+            session_id = ""
+            # If starts with session=, remove it
+            if f.read().startswith("session="):
+                f.seek(8)
+            session_id = f.read().strip()
+    return session_id
 
 def get_url(year, day):
     return f"https://adventofcode.com/{year}/day/{day}/input"
 
-SESSION = get_session_id(session_cookie)
+SESSION = get_session_id()
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
@@ -33,9 +38,15 @@ COOKIES = {"session": SESSION}
 
 
 def get_input(day):
-    path = f"inputs/{day:02d}"
+    input_file = f"{current_year}/{day:01d}/inputs.txt"
+    path = os.path.join(current_directory, f"{current_year}/{day:01d}")
 
-    if not exists(path):
+    # Create the path if it doesn't exist
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+        
+    # Check if the file exists before trying to open it
+    if not os.path.exists(input_file):
         url = get_url(current_year, current_day)
         print(f"Downloading input from {url}")
         response = requests.get(url, headers=HEADERS, cookies=COOKIES)
@@ -43,8 +54,8 @@ def get_input(day):
             raise RuntimeError(
                 f"Request failed\n\tstatus code: {response.status_code}\n\tmessage: {response.content}"
             )
-        with open(path, "w") as f:
+        with open(input_file, "w") as f:
             f.write(response.text[:-1])
 
-    with open(path, "r") as f:
+    with open(input_file, "r") as f:
         return f.read()
--- setup/utils/new_file.py
+++ setup/utils/new_file.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 from datetime import datetime
 from api import get_input
 
@@ -11,24 +13,52 @@ path = f"{current_year}/{current_day}/src"
 # Create the path if it doesn't exist
 if not os.path.exists(path):
     os.makedirs(path, exist_ok=True)
-    
-l = filter(lambda x: "__" not in x and ".py" in x, os.listdir(path))
-l = list(l)
-n = int(sorted(l)[-1][:2]) + 1 if len(l) > 0 else 1
 
+# Get the next number for the file
+files = [x for x in os.listdir(path) if "__" not in x and x.endswith('.py')]
+if files:
+    match = re.search(r'(\d+)\.py$', files[-1])
+    n = int(match.group(1)) + 1 if match is not None else 1
+else:
+    n = 1
+    
 # Generated the input file
-input_str = get_input(n)
+input_str = get_input(current_day)
 with open(f"{current_year}/{current_day}/input.txt", "w") as f:
     f.write(input_str)
     
 # Copy contents of template file
-DEFAULT_FILE = ""
-with open("python_templ.py", "r") as f:
-    DEFAULT_FILE = f.read()
+DEFAULT_FILE = f"""
+from aocd.models import Puzzle
+import re
 
-path = f"{current_year}/{current_day}/src/{n:02}.py"
-    
-with open(path, "w") as f:
-    f.write(DEFAULT_FILE)
+puzzle = Puzzle(year={current_year}, day={current_day})
+data = puzzle.input_data
+# print(data)
+
+def part1(data):
+    pass    
+
+def part2(data):
+    pass
+
+part1_answer = part1(data)
+print(f"Part 1 answer: {{part1_answer}}")
+
+# puzzle.answer_a = part1_answer
+
+# part2_answer = part2(data)
+# print(f"Part 2 answer: {{part2_answer}}")
+# puzzle.answer_b = part2_answer
+"""
+# with open("python_templ.py", "r") as f:
+#     DEFAULT_FILE = f.read()
+
+path = f"{current_year}/{current_day}/src/solver.py"
+
+# Create the file if it doesn't exist
+if not os.path.exists(path):
+    with open(path, "w") as f:
+        f.write(DEFAULT_FILE)
 
 print(f"Enter your solution in {path}")
